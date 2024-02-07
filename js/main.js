96bd12156ca3f84bc0160f21d86559c7eb380495
Shlomi-Afrimi <159270608+Shlomi-Afrimi@users.noreply.github.com>
2024-02-07 16:04:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'set strict'
+
+// gBoard – A Matrix containing cell objects: Each cell:
+//  { minesAroundCount: 4, isShown: false, isMine: false, isMarked: true }
+
+// gLevel = { SIZE: 4, MINES: 2 }
+
+// gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
+
+var gBoard[]
+var gLevel[] = [ {}
+
+]
+
+function onInit(){
+
+}
+
+function buildBoard(){
+
+}
+
+function setMinesNegsCount(board){
+
+}
+
+function renderBoard(board){
+
+}
+
+function onCellClicked(){
+
+}
+
+function onCellMarked(elCell){
+
+}
+
+function checkGameOver(){
+
+}
+
+function expandShown(board, elCell, i, j){
+
+}
+
---
+++ js/util.js
@@ -0,0 +1,24 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+     // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+     // The maximum is inclusive and the minimum is inclusive
+}
+
+  
